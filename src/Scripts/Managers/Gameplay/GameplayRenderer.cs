785324b44b865a1bc3fe52ee2afe2eaf8db499de
hexlog12-dev <hexlog12@gmail.com>
2026-04-28 03:40:59 +0900

    Update GameplayRenderer.cs
    better perfomance

--- src/Scripts/Managers/Gameplay/GameplayRenderer.cs
+++ src/Scripts/Managers/Gameplay/GameplayRenderer.cs
@@ -16,12 +16,18 @@ public partial class GameplayRenderer : ColorRect
 		_shaderMaterial = GetMaterial() as ShaderMaterial;
 	}
 
+
+    
+    static readonly StringName GAME_TEXTURE = new StringName("game_texture);
+    static readonly StringName SIGHT_TEXTURE = new StringName("sight_texture);
+	static readonly StringName WORLD_TEXTURE = new StringName("world_texture);
+	
 	public override void _Process(double delta)
 	{
 		if (_gameViewport == null || _sightViewport == null || _worldViewport == null || _shaderMaterial == null) return;
 
-		_shaderMaterial.SetShaderParameter("game_texture", _gameViewport.GetTexture());
-		_shaderMaterial.SetShaderParameter("sight_texture", _sightViewport.GetTexture());
-		_shaderMaterial.SetShaderParameter("world_texture", _worldViewport.GetTexture());
+		_shaderMaterial.SetShaderParameter(GAME_TEXTURE, _gameViewport.GetTexture());
+		_shaderMaterial.SetShaderParameter(SIGHT_TEXTURE, _sightViewport.GetTexture());
+		_shaderMaterial.SetShaderParameter(WORLD_TEXTURE, _worldViewport.GetTexture());
 	}
 }
